e93c4e193e899e3a84e1805e811e525a6f151853
Hasan <hasan.quliyev.2003@gmail.com>
2022-07-07 21:43:07 +0400

    ilk commit

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
       System.out.println("Hello World");
-    , System.out.println(x:"Goodbye World");
+     System.out.println("Goodbye World");
+     System.out.println("Salam ");
+     
     } 
 }
